2ea19f3e6ad9d9a724ec4892dcdad8fa0f6845d3
Zander Bolgar <lezed1@users.noreply.github.com>
2026-04-13 11:14:45 -0400

    Basic search box (#4)
    * Basic search box
    
    * Small formatting fixes

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,31 @@
+import { observer } from "mobx-react-lite";
+import { useStore } from "#/stores/StoreContext.tsx";
+
+const SearchBox = observer(() => {
+  const store = useStore();
+
+  return (
+    <div className="flex flex-col w-full">
+      <h2 className="text-shadow-md px-4 py-2 bg-[#28221d] rounded-t border-b border-[#806f61] shadow-lg">
+        Search Demonic Pacts
+      </h2>
+      <div className="w-full px-4 p-2 flex items-center gap-2">
+        <input
+          type="text"
+          className="form-control rounded flex-3"
+          value={store.searchQuery}
+          placeholder="Search pact descriptions..."
+          onChange={(e) => (store.searchQuery = e.target.value)}
+        />
+        <div className="flex-2 text-right text-sm">
+          {store.searchQuery &&
+            (store.nodesMatchingSearch.size > 0
+              ? `${store.nodesMatchingSearch.size} highlighted`
+              : "No matches")}
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default SearchBox;
